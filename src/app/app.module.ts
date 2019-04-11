a49d99f73e19f144319592b749367a7c201a95cb
karolkkk <s15195@pjwstk.edu.pl>
2019-04-11 10:30:54 +0200

    sth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+   routingComponents
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
